5932ecaa27cc23de31f7d192fa6123ded089c0ce
everyoneselectronic <fogertyliam@gmail.com>
2018-01-02 22:29:59 +0000

    fixed work page
    displays info correctly

--- loop-templates/content-work.php
+++ loop-templates/content-work.php
@@ -29,7 +29,7 @@
 			echo '<div class="owl-carousel owl-centered owl-theme">';
 			$num = 1;
 			$totalSlides = 0;
-			$firstCaption = "test";
+			$firstCaption = array();
 
 		 	// loop through the rows of data
 		    while ( have_rows('media-gallery') ) : the_row();
@@ -55,6 +55,7 @@
 					    	</div>';
 					    	$num++;
 							$totalSlides++;
+							array_push($firstCaption, $imgCAP);
 					    }
 
 					endif;
@@ -62,13 +63,15 @@
 		        endif;
 
 		        if( get_row_layout() == 'video' ):
-		        	echo '<div class="item" data-slide-number="' . $num . '" data-caption="video"><div class="embed-container">';
-		        	
-					the_sub_field('video');
-
+		        	$videoCAP = get_sub_field('video-caption');
+		        	echo '<div class="item" data-slide-number="' . $num . '" data-caption="' . $videoCAP . '"><div class="embed-container">';
+						the_sub_field('video');
 					echo '</div></div>';
 		        	$num++;
 					$totalSlides++;
+
+					array_push($firstCaption, $videoCAP);
+
 		        endif;
 
 		        
@@ -80,7 +83,7 @@
 			echo
 	    	'<div class="owl-information">
 	    		<div class="owl-number"><p><span class="owl-number-data">1</span> of '. $totalSlides .'</p></div>
-	    		<div class="owl-caption"><p>' . $images[0]['caption'] . '</p></div>
+	    		<div class="owl-caption"><p>' . $firstCaption[0] . '</p></div>
 	    	</div>';
 
 		endif;
@@ -93,13 +96,13 @@
 	<div class="col-md-10 col-md-push-1">
 
 		<div class="work-info">
-			<p><span class="medium-title">Medium: </span><span class=""><?php the_field('medium'); ?></span></p>
-			<p><span class="format-title">Format: </span><span class=""><?php the_field('format'); ?></span></p>
-			<p><span class="size-title">Size: </span><span class=""><?php the_field('size'); ?></span></p>
-			<p><span class="show-title">Show: </span><span class=""><?php the_field('show'); ?></span></p>
-			<p><span class="venue-title">Venue: </span><span class=""><?php the_field('venue'); ?></span></p>
-			<p><span class="year-title">Year: </span><span class=""><?php the_field('year'); ?></span></p>
-			<p><span class="duration-title">Duration: </span><span class=""><?php the_field('duration'); ?></span></p>
+			<?php if ( get_field('medium') ): ?><p><span class="medium-title">Medium: </span><span class=""><?php the_field('medium'); ?></span></p><?php endif ?>
+			<?php if ( get_field('format') ): ?><p><span class="format-title">Format: </span><span class=""><?php the_field('format'); ?></span></p><?php endif ?>
+			<?php if ( get_field('size') ): ?><p><span class="size-title">Size: </span><span class=""><?php the_field('size'); ?></span></p><?php endif ?>
+			<?php if ( get_field('show') ): ?><p><span class="show-title">Show: </span><span class=""><?php the_field('show'); ?></span></p><?php endif ?>
+			<?php if ( get_field('venue') ): ?><p><span class="venue-title">Venue: </span><span class=""><?php the_field('venue'); ?></span></p><?php endif ?>
+			<?php if ( get_field('year') ): ?><p><span class="year-title">Year: </span><span class=""><?php the_field('year'); ?></span></p><?php endif ?>
+			<?php if ( get_field('duration') ): ?><p><span class="duration-title">Duration: </span><span class=""><?php the_field('duration'); ?></span></p><?php endif ?>
 		</div>
 	    
 		<div class="entry-content columnizer" style="clear:both;">
